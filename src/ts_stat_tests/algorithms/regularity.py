1796788492b1ef4891223b5ebdcdc50d51dc9a4f
chrimaho <44449504+chrimaho@users.noreply.github.com>
2026-01-06 06:58:29 +1100

    Add regularity entropy algorithms - Implement `approx_entropy()`, `sample_entropy()`, `permutation_entropy()`, and `spectral_entropy()` functions - Utilise the `antropy` library for underlying entropy calculations - Apply the `typechecked` decorator to ensure parameter type safety - Define the `__all__` list to expose the public API of the module

---
+++ src/ts_stat_tests/algorithms/regularity.py
@@ -0,0 +1,84 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#    Setup                                                                  ####
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+# Imports                                                                   ####
+# ---------------------------------------------------------------------------- #
+
+
+# ## Python StdLib Imports ----
+from typing import Optional, Union
+
+# ## Python Third Party Imports ----
+import numpy as np
+from antropy import (
+    app_entropy as a_app_entropy,
+    perm_entropy as a_perm_entropy,
+    sample_entropy as a_sample_entropy,
+    spectral_entropy as a_spectral_entropy,
+)
+from numpy.typing import ArrayLike
+from typeguard import typechecked
+
+
+# ---------------------------------------------------------------------------- #
+# Exports                                                                   ####
+# ---------------------------------------------------------------------------- #
+
+
+__all__: list[str] = [
+    "approx_entropy",
+    "sample_entropy",
+    "spectral_entropy",
+    "permutation_entropy",
+]
+
+
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+#    Algorithms                                                             ####
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+
+@typechecked
+def approx_entropy(x: ArrayLike, order: int = 2, metric: str = "chebyshev") -> float:
+    return a_app_entropy(x=x, order=order, metric=metric)
+
+
+@typechecked
+def sample_entropy(x: ArrayLike, order: int = 2, metric: str = "chebyshev") -> float:
+    return a_sample_entropy(x=x, order=order, metric=metric)
+
+
+@typechecked
+def permutation_entropy(
+    x: ArrayLike,
+    order: int = 3,
+    delay: Union[int, list, np.ndarray] = 1,
+    normalize: bool = False,
+):
+    return a_perm_entropy(x=x, order=order, delay=delay, normalize=normalize)
+
+
+@typechecked
+def spectral_entropy(
+    x: ArrayLike,
+    sf: float = 1,
+    method: str = "fft",
+    nperseg: Optional[int] = None,
+    normalize: bool = False,
+    axis: int = -1,
+) -> Union[float, np.ndarray]:
+    return a_spectral_entropy(
+        x=x,
+        sf=sf,
+        method=method,
+        nperseg=nperseg,
+        normalize=normalize,
+        axis=axis,
+    )
